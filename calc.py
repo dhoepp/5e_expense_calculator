b048edc6c9b6bb466b46f6c799f179a91d52b7ad
Dustin Hoeppner <dhoepp@me.com>
2026-03-01 19:25:39 -0600

    Refactor calculation logic to incorporate ability checks and improve modifier calculation

--- calc.py
+++ calc.py
@@ -1,3 +1,5 @@
+import random
+
 def calculate():
     modifier = 0
     baseline = 350
@@ -7,10 +9,32 @@ def calculate():
     for project in range(n):
         projectSuccess = 0
         for skill in range(3): # Each project has 3 ability checks
-            roll = int(input(f"ability score {project + 1}.{skill + 1}? ")) # Appends the project and skill check for each iteration
-            projectSuccess += roll
-        projectAvg = projectSuccess / 3
-        modifier += projectAvg
+            roll_input = ""
+            contest = random.randint(1, 10) + random.randint(1, 10) + 4
+            roll_input = input(f"ability check {project + 1}.{skill + 1}? | contest = {contest} (Y/n) ") # Appends the project and skill check for each iteration
+            
+            #successed are discretionary by the DM
+            if roll_input == "n" or roll_input == "N":
+                projectSuccess = projectSuccess
+            else:
+                projectSuccess += 1
+        
+        modifier_input = 0
+        
+        #dice roll is determined by number of successes
+        if projectSuccess == 1:
+            modifier_input = int(input(f"d8 roll for Project {project + 1}? "))
+        elif projectSuccess == 2:
+            modifier_input = int(input(f"d10 roll for Project {project + 1}? "))
+        elif projectSuccess == 3:
+            modifier_input = int(input(f"d12 roll for Project {project + 1}? "))
+        else:
+            print(f"Project Success = {projectSuccess}")
+
+            
+        modifier += modifier_input
+
+    print(f"modifier = {modifier}")
 
     d100 = int(input("d100 roll? "))
 
@@ -19,14 +43,16 @@ def calculate():
         baseline = int(baseline_input)
 
     multiplier = (d100 + modifier - 50) / 33 
+    print(f"multiplier = {multiplier:.2f}")
 
     result = baseline * multiplier
+    print(f"result = {baseline} * {multiplier:.2f}")
     result = round(result /25) * 25 # Round to the nearest 25
 
     if result >= 0:
         print(f"Your monthly profits are {result} gold")
     else:
-        print(f"Your monthly expenses are (-{abs(result)}) gold")
+        print(f"Your monthly expenses are ({result}) gold")
 
 
 if __name__ == "__main__":
